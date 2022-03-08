339a7c5551ff807f07ffc8524094fb6ed82fbcdd
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-08 20:35:28 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,9 +1,19 @@
 /*
 	WAP to calculate simple interest.
 */
-public class ex1{
+import java.util.Scanner;
+public class ex2{
 	public static void main(String[] args){
-		int sal=5000;
-		System.out.println("His salary is : "+sal);
+		Scanner sc = new Scanner(System.in);
+		int n;
+		float p,r,tot;
+		System.out.println("Enter a amount : ");
+		n = sc.nextInt();
+		System.out.println("Enter a reat of amount : ");
+		p = sc.nextFloat();
+		System.out.println("Enter a year of amount : ");
+		r = sc.nextFloat();
+		tot = (p*r*n)/100;
+		System.out.println("Your simple interest is : "+tot);		
 	}
 }
