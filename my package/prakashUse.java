9f845e3694db1a516058b3586ca6da8425a8f7f4
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-12 08:56:03 +0530

    Add files via upload

---
+++ my package/prakashUse.java
@@ -0,0 +1,11 @@
+// pm(prakash) package use
+import pm.prakash;
+public class prakashUse
+{
+	public static void main(String[] args)
+	{
+		String definition = "package use here...";
+		prakash p = new prakash();
+		p.detail(definition);
+	}
+}
