be36a522eea4d38c6600e6373632e8cf6e3f6086
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-18 14:53:15 +0530

    Add files via upload

---
+++ staticImport.java
@@ -0,0 +1,12 @@
+// Static Import 
+import java.lang.System.*;//Normal import
+import static java.lang.System.*;//Static import
+
+class staticImport
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Normal Import");
+		out.println("Static Import");
+	}
+}
