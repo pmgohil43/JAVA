68d5c52aa7222acc65bcee71a34d8f123f5e3d95
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-30 15:46:37 +0530

    Delete arith.java

--- arith.java
+++
@@ -1,9 +0,0 @@
-public class arith{
-	public static void main(String args[]){
-		int n1 = Integer.parseInt(args[0]);
-		int n2 = Integer.parseInt(args[1]);
-		
-		int add = n1 + n2;
-		System.out.println("Addition : " + add);
-	}
-}
