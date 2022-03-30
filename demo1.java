26cd56a5fee2bc898393beb4ebabe402b80aa7b5
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-30 16:10:21 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,28 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class demo1 extends Applet{
+	public void init(){
+		
+	}
+	
+	public void start(){
+		
+	}
+	
+	public void paint(Graphics g){
+		g.drawString("Prakash Gohil",50,25);
+	}
+	
+	public void stop(){
+		
+	}
+	
+	public void destroy(){
+		
+	}
+}
+/*
+javac demo1.java
+appletviewer demo1.html
+*/
