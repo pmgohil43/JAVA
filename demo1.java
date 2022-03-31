f7a199405dde9edadaec791830f2cce93a20bde2
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-31 12:11:13 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -1,28 +1,26 @@
-import java.applet.Applet;
-import java.awt.Graphics;
+import java.applet.*;
+import java.awt.*;
 
 public class demo1 extends Applet{
-	public void init(){
-		
-	}
-	
-	public void start(){
-		
-	}
-	
 	public void paint(Graphics g){
-		g.drawString("Prakash Gohil",50,25);
-	}
-	
-	public void stop(){
 		
-	}
-	
-	public void destroy(){
-		
-	}
+		g.drawOval(150,50,100,100);
+		//left eye
+		g.drawOval(165,75,20,20);
+		g.drawArc(155,70,40,40,60,60);
+		//right eye
+		g.drawOval(215,75,20,20);
+		g.drawArc(205,70,40,40,60,60);
+		//nose
+		g.drawLine(200,95,195,120);
+		g.drawLine(213,117,195,120);
+		//lips
+		g.drawArc(175,100,50,40,215,120);
+		g.drawArc(175,110,50,30,215,120);
+	}	
 }
+
 /*
-javac demo1.java
-appletviewer demo1.html
+<applet code="demo1.class" height="400" width="1000" alt="Error loading applet!"></applet>
+<h1>Prakash</h1>
 */
