5b2b17de600c32aca1c3aa55255f03975fe3d9d3
Pm Gohil <94962564+pmgohil43@users.noreply.github.com>
2022-03-31 15:00:32 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class demo1 extends Applet{
 	public void paint(Graphics g){
 		
+		//head
 		g.drawOval(150,50,100,100);
 		//left eye
 		g.drawOval(165,75,20,20);
@@ -17,10 +18,25 @@ public class demo1 extends Applet{
 		//lips
 		g.drawArc(175,100,50,40,215,120);
 		g.drawArc(175,110,50,30,215,120);
-	}	
+		//neck
+		g.drawLine(180,190,180,145);
+		g.drawLine(220,190,220,145);
+		//body
+		Color color2 = new Color(250,88,245);
+		g.setColor(color2);
+		g.drawRoundRect(145,190,110,180,20,20);
+		//shirt design
+		Color color1 = new Color(25,88,245);
+		g.setColor(color1);
+		g.drawRoundRect(145,190,110,180,100,100);
+		Color color = new Color(34,139,34);
+		g.setColor(color);
+		g.fillOval(150,230,100,100);
+		//pent design
+	}
 }
 
 /*
-<applet code="demo1.class" height="400" width="1000" alt="Error loading applet!"></applet>
+<applet code="demo1.class" height="800" width="400" alt="Error loading applet!"></applet>
 <h1>Prakash</h1>
 */
